4211a82c02a9d81763674e1927a62680c8b1e427
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2020-04-05 12:26:49 +0300

    Create basic app with test

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@testing-library/jest-dom/extend-expect'
+
+import Warehouses from '../components/warehouses'
+
+test('Warehouses component is correct', () => {
+    const container = document.createElement('div')
+    ReactDOM.render(<Warehouses/>, container)
+    expect(container.querySelector('.content')).toBeTruthy()
+    expect(container.querySelector('.content')).toHaveTextContent('Warehouses are here!')
+})
