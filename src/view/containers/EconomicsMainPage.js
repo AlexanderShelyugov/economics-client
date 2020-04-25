16655c1fe89d5695f3b3bdd6c8c21e2e5d26a206
Alexander Shelyugov <62933903+AlexanderShelyugov@users.noreply.github.com>
2020-04-25 19:45:09 +0300

    Setup basic navigation bar

--- src/view/containers/EconomicsMainPage.js
+++ src/view/containers/EconomicsMainPage.js
@@ -1,102 +1,120 @@
-import _ from 'lodash'
-import { Button, Container, Drawer, IconButton, Typography } from '@material-ui/core'
-import CloseIcon from '@material-ui/icons/Close'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import { AppBar, Divider, Drawer, IconButton, List, ListItem, ListItemText, Toolbar, Typography } from '@material-ui/core'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import HomeIcon from '@material-ui/icons/Home'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import MenuIcon from '@material-ui/icons/Menu'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import StoreIcon from '@material-ui/icons/Store'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import React from 'react'
 
-import WarehouseSummary from '../components/WarehouseSummary'
-import WarehousesTable from './WarehousesTable'
-import { warehouseOperations as operations, warehouseSelectors as selectors } from '../../state/ducks/warehouse'
+const menuWidth = 240
 
-class EconomicsMainPage extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-        this.handleRefreshClick = this.handleRefreshClick.bind(this)
-        this.handleWarehouseClick = this.handleWarehouseClick.bind(this)
-    }
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    drawer: {
+        width: menuWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: menuWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+}));
 
-    componentDidMount() {
-        const { dispatch } = this.props
-        dispatch(operations.getWarehouses())
-    }
 
-    handleRefreshClick = e => {
-        e.preventDefault()
-        const { dispatch } = this.props
-        dispatch(operations.invalidateWarehouses())
-        dispatch(operations.getWarehouses())
-    }
+export default function EconomicsMainPage() {
+    const classes = useStyles()
+    const theme = useTheme()
+    const [open, setOpen] = React.useState(false)
 
-    setOpen(isOpen) {
-        this.setState(() => ({ isOpen }))
-    }
+    const handleMenuOpen = () => {
+        setOpen(true);
+    };
 
-    getOpen() {
-        return this.state.isOpen
-    }
+    const handleMenuClose = () => {
+        setOpen(false);
+    };
 
-    handleWarehouseClick = id => {
-        if (_.get(this.state, "selectedWarehouse.id") === id) {
-            this.setOpen(!this.getOpen())
-            return
-        }
-        this.setState({
-            selectedWarehouse: selectors.getById(id)
-        })
-        this.setOpen(true)
-    }
-
-    handleDrawerClose = e => {
-        e.preventDefault()
-        this.setOpen(false)
-    }
-
-    render() {
-        const { value, message } = this.props
-        const { selectedWarehouse, isOpen } = this.state
-        return (
-            <div>
-                <Button variant="contained" color="primary" onClick={this.handleRefreshClick} >Refresh</Button>
-                {
-                    message == null
-                        ? null
-                        : <Typography variant="h4">{message}</Typography>
-                }
-                {
-                    _.isEmpty(value)
-                        ? <h2>No warehouses</h2>
-                        :
-                        <div>
-                            <main>
-                                <WarehousesTable
-                                    onWarehouseClick={this.handleWarehouseClick}
-                                />
-                            </main>
-                            <aside>
-                                <Drawer anchor="right"
-                                    variant="persistent"
-                                    open={isOpen}>
-                                    <div>
-                                        <IconButton onClick={this.handleDrawerClose}>
-                                            <CloseIcon />
-                                        </IconButton>
-                                    </div>
-                                    <Container>
-                                        <WarehouseSummary value={selectedWarehouse} />
-                                    </Container>
-                                </Drawer>
-                            </aside>
-                        </div>
-                }
-            </div>
-        )
-    }
+    return (
+        <div className="{classes.root}">
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className="{classes.menuButton}"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={handleMenuOpen}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" className="{classes.title}">
+                        Economics
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="left"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleMenuClose}>
+                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>
+                    <ListItem
+                        button
+                        key={1}
+                        component="a"
+                        href="/"
+                    >
+                        <ListItemIcon><HomeIcon /></ListItemIcon>
+                        <ListItemText primary="Home" />
+                    </ListItem>
+                    <ListItem
+                        button
+                        key={2}
+                        component="a"
+                        href="/warehouses"
+                    >
+                        <ListItemIcon><StoreIcon /></ListItemIcon>
+                        <ListItemText primary="Warehouses" />
+                    </ListItem>
+                    <ListItem
+                        button
+                        key={3}
+                        component="a"
+                        href="/products"
+                    >
+                        <ListItemIcon><ShoppingCartIcon /></ListItemIcon>
+                        <ListItemText primary="Products" />
+                    </ListItem>
+                </List>
+            </Drawer>
+        </div>
+    )
 }
-
-const mapStateToProps = state => ({
-    value: state.warehouse.entities.byId,
-    message: state.warehouse.meta.message
-})
-
-export default connect(mapStateToProps)(EconomicsMainPage)
